78b4eed462d833d5720d2b4bfb41fbf3ad6de7e4
prodzombie <emilio.palmerini@gmail.com>
2026-08-29 21:37:29 +0200

    Package taskwarrior-web reproducibly with Nix

--- build.rs
+++ build.rs
@@ -8,17 +8,17 @@
  * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-use std::process::Command;
+use std::{env, process::Command};
 
 fn main() {
     println!("cargo:rerun-if-changed=frontend/");
-    println!("Install npm dependencies");
-    if !Command::new("npm")
-        .current_dir("frontend")
-        .args(["install"])
-        .status()
-        .unwrap()
-        .success()
+    if env::var("TASKWARRIOR_WEB_SKIP_NPM_INSTALL").as_deref() != Ok("1")
+        && !Command::new("npm")
+            .current_dir("frontend")
+            .args(["ci"])
+            .status()
+            .unwrap()
+            .success()
     {
         panic!("Could not install dependencies")
     }
---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Minimalistic web UI for Taskwarrior";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          taskwarrior-web = pkgs.rustPlatform.buildRustPackage (finalAttrs: {
+            pname = "taskwarrior-web";
+            version = "2.0.1";
+
+            src = self;
+            cargoLock.lockFile = ./Cargo.lock;
+
+            npmDeps = pkgs.fetchNpmDeps {
+              src = ./frontend;
+              hash = "sha256-tyUSP6khP2lxpHzdRYDy0BgOa2STk8q1r9zOZcTIiPY=";
+            };
+            npmRoot = "frontend";
+
+            nativeBuildInputs = [
+              pkgs.nodejs
+              pkgs.npmHooks.npmConfigHook
+            ];
+
+            env.TASKWARRIOR_WEB_SKIP_NPM_INSTALL = "1";
+
+            meta = {
+              description = "Minimalistic web UI for Taskwarrior";
+              homepage = "https://github.com/prodzombie/taskwarrior-web";
+              license = pkgs.lib.licenses.mit;
+              mainProgram = "taskwarrior-web";
+              platforms = pkgs.lib.platforms.linux;
+            };
+          });
+        in
+        {
+          inherit taskwarrior-web;
+          default = taskwarrior-web;
+        });
+    };
+}
