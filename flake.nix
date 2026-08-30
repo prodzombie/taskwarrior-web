61cefc4acf0c13295138aaa68ccccca96396b045
prodzombie <emilio.palmerini@gmail.com>
2026-08-30 07:16:56 +0200

    Provide a reproducible direnv development shell

--- flake.nix
+++ flake.nix
@@ -5,46 +5,75 @@
 
   outputs = { self, nixpkgs }:
     let
-      systems = [ "x86_64-linux" ];
+      systems = [ "aarch64-darwin" "x86_64-linux" ];
+      packageSystems = [ "x86_64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
+      mkTaskwarriorWeb = pkgs: pkgs.rustPlatform.buildRustPackage (finalAttrs: {
+        pname = "taskwarrior-web";
+        version = "2.0.1";
+
+        src = self;
+        cargoLock.lockFile = ./Cargo.lock;
+
+        npmDeps = pkgs.fetchNpmDeps {
+          src = ./frontend;
+          hash = "sha256-tyUSP6khP2lxpHzdRYDy0BgOa2STk8q1r9zOZcTIiPY=";
+        };
+        npmRoot = "frontend";
+
+        nativeBuildInputs = [
+          pkgs.nodejs
+          pkgs.npmHooks.npmConfigHook
+        ];
+        nativeCheckInputs = [ pkgs.bash ];
+
+        env.TASKWARRIOR_WEB_SKIP_NPM_INSTALL = "1";
+
+        meta = {
+          description = "Minimalistic web UI for Taskwarrior";
+          homepage = "https://github.com/prodzombie/taskwarrior-web";
+          license = pkgs.lib.licenses.mit;
+          mainProgram = "taskwarrior-web";
+          platforms = pkgs.lib.platforms.linux;
+        };
+      });
     in
     {
-      packages = forAllSystems (system:
+      packages = nixpkgs.lib.genAttrs packageSystems (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          taskwarrior-web = pkgs.rustPlatform.buildRustPackage (finalAttrs: {
-            pname = "taskwarrior-web";
-            version = "2.0.1";
-
-            src = self;
-            cargoLock.lockFile = ./Cargo.lock;
-
-            npmDeps = pkgs.fetchNpmDeps {
-              src = ./frontend;
-              hash = "sha256-tyUSP6khP2lxpHzdRYDy0BgOa2STk8q1r9zOZcTIiPY=";
-            };
-            npmRoot = "frontend";
-
-            nativeBuildInputs = [
-              pkgs.nodejs
-              pkgs.npmHooks.npmConfigHook
-            ];
-            nativeCheckInputs = [ pkgs.bash ];
-
-            env.TASKWARRIOR_WEB_SKIP_NPM_INSTALL = "1";
-
-            meta = {
-              description = "Minimalistic web UI for Taskwarrior";
-              homepage = "https://github.com/prodzombie/taskwarrior-web";
-              license = pkgs.lib.licenses.mit;
-              mainProgram = "taskwarrior-web";
-              platforms = pkgs.lib.platforms.linux;
-            };
-          });
+          taskwarrior-web = mkTaskwarriorWeb pkgs;
         in
         {
           inherit taskwarrior-web;
           default = taskwarrior-web;
+        });
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          taskwarrior-web = mkTaskwarriorWeb pkgs;
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.cargo
+              pkgs.clippy
+              pkgs.nodejs
+              pkgs.rustc
+              pkgs.rustfmt
+              pkgs.taskwarrior3
+            ];
+
+            npm_config_cache = taskwarrior-web.npmDeps;
+            TASKWARRIOR_WEB_SKIP_NPM_INSTALL = "1";
+
+            shellHook = ''
+              if [[ ! -d frontend/node_modules ]]; then
+                npm ci --prefix frontend --offline
+              fi
+            '';
+          };
         });
     };
 }
